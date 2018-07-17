99d59d2d997ed452050a5606f997ff69e78a0d28
AndrewM <andrewmadsen10@gmail.com>
2018-07-17 12:27:30 -0600

    Added drag and drop recognizer

--- Fireworks 2.0/GameScene.swift
+++ Fireworks 2.0/GameScene.swift
@@ -10,23 +10,63 @@ import SpriteKit
 import GameplayKit
 
 
-class GameScene: SKScene
-{
-    let booms: [SKEmitterNode?] = [SKEmitterNode(fileNamed: "FireworkExplosion"), SKEmitterNode(fileNamed: "FireworkExplosion2"),SKEmitterNode(fileNamed:"FireworkExplosion3"), SKEmitterNode(fileNamed:  " FireworkExplosion4")]
+class GameScene: SKScene {
+    var booms: [SKEmitterNode] = [SKEmitterNode(fileNamed: "FireworkExplosion")!, SKEmitterNode(fileNamed: "FireworkExplosion2")!, SKEmitterNode(fileNamed:"FireworkExplosion3")!, SKEmitterNode(fileNamed: "FireworkExplosion4")!]
+    
     var hasSetSize = false
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?)
-    {
-        for touch in touches
-        {
-            if let particles = self.booms[Int(arc4random_uniform(UInt32(booms.count)))]?.copy() as! SKEmitterNode?
+    var particles = [UITouch: SKEmitterNode]()
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        fireFirework(at: touches)
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        fireFirework(at: touches)
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        removeTouches(touches)
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        removeTouches(touches)
+    }
+    
+    func removeTouches(_ touches: Set<UITouch>) {
+        for touch in touches {
+        particles[touch] = nil
+        }
+    }
+    
+    func getParticle(_ touch: UITouch) -> SKEmitterNode? {
+        if let particle = particles[touch] {
+            return particle.copy() as? SKEmitterNode
+        } else {
+            
+            let randomNumber = Int(arc4random_uniform(UInt32(booms.count)))
+            if let particle = self.booms[randomNumber].copy() as? SKEmitterNode
             {
-                let point = touch.preciseLocation(in: touch.view)
-                particles.position = CGPoint(x: point.x, y: -point.y)
-                self.addChild(particles)
+                particles[touch] = particle
+                return particle
+            } else {
+                return nil
             }
         }
     }
-
+    func fireFirework(at touches: Set<UITouch>) {
+        for touch in touches
+        {
+            if let particle = getParticle(touch) {
+                
+                let point = touch.preciseLocation(in: touch.view)
+                particle.position = CGPoint(x: point.x, y: -point.y)
+                self.addChild(particle)
+            } else {
+                print("No Particle Found")
+            }
+        }
+    }
+    
     override func update(_ currentTime: TimeInterval)
     {
         if !hasSetSize
