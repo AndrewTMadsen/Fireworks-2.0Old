a5143a5b2b253257203d67921e9dcdf827bcd9c5
AndrewM <andrewmadsen10@gmail.com>
2018-07-31 08:03:40 -0600

    Forgot to update

--- Fireworks 2.0/GameScene.swift
+++ Fireworks 2.0/GameScene.swift
@@ -8,25 +8,45 @@
 
 import SpriteKit
 import GameplayKit
+import AVFoundation
 
 class GameScene: SKScene {
     
     var hasSetSize = false
     var particles = [UITouch: SKEmitterNode]()
     var particleTrails = [UITouch: SKEmitterNode]()
+    var boomPlayer: [AVAudioPlayer] = []
+    var countdownTimer: Timer!
+    var delayTime = 0.125
+    var isTouchEligible = true
     
-    var booms: [SKEmitterNode] = [SKEmitterNode(fileNamed: "FireworkExplosion")!, SKEmitterNode(fileNamed: "FireworkExplosion2")!, SKEmitterNode(fileNamed:"FireworkExplosion3")!, SKEmitterNode(fileNamed: "FireworkExplosion4")!, SKEmitterNode(fileNamed: "FireWorkAN")!, SKEmitterNode(fileNamed: "FireworkRL")!, SKEmitterNode(fileNamed: "FireworkKA")!, SKEmitterNode(fileNamed: "FireworkCA")!, SKEmitterNode(fileNamed: "FireworkAB")!, SKEmitterNode(fileNamed: "FireworkSM")!, SKEmitterNode(fileNamed: "FireworkBM")!]
+    func startTimer() {
+        countdownTimer = Timer.scheduledTimer(timeInterval: delayTime, target: self, selector: #selector(endTimer), userInfo: nil, repeats: true)
+        isTouchEligible = false
+    }
+
+    @objc func endTimer() {
+        countdownTimer.invalidate()
+        isTouchEligible = true
+    }
     
     //MARK: Touch
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        startTail(at: touches) { point in
-            self.fireFirework(point)
+        if isTouchEligible {
+            startTail(at: touches) { point in
+                self.fireFirework(point)
+                self.playBoomSound()
+            }
         }
     }
-    
-    //override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-   //     fireFirework(at: touches)
-   // }
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if isTouchEligible {
+            startTail(at: touches) { point in
+                self.fireFirework(point)
+                self.playBoomSound()
+            }
+        }
+    }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         removeTouches(touches)
@@ -34,6 +54,57 @@ class GameScene: SKScene {
     
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
         removeTouches(touches)
+    }
+    
+    func removeTouches(_ touches: Set<UITouch>) {
+        for touch in touches {
+            particles[touch] = nil
+        }
+    }
+    
+    //MARK: Fireworks
+    
+    var booms: [SKEmitterNode] = [SKEmitterNode(fileNamed: "FireworkExplosion")!, SKEmitterNode(fileNamed: "FireworkExplosion2")!, SKEmitterNode(fileNamed:"FireworkExplosion3")!, SKEmitterNode(fileNamed: "FireworkExplosion4")!, SKEmitterNode(fileNamed: "FireWorkAN")!, SKEmitterNode(fileNamed: "FireworkRL")!, SKEmitterNode(fileNamed: "FireworkKA")!, SKEmitterNode(fileNamed: "FireworkCA")!, SKEmitterNode(fileNamed: "FireworkAB")!, SKEmitterNode(fileNamed: "FireworkSM")!, SKEmitterNode(fileNamed: "FireworkBM")!]
+   
+    
+//    func fireFirework(at touches: Set<UITouch>) {
+//        for touch in touches {
+//            if let particle = getParticle(touch) {
+//
+//                let point = touch.preciseLocation(in: touch.view)
+//                particle.position = CGPoint(x: point.x, y: -point.y)
+//                self.addChild(particle)
+//            } else {
+//                print("No Particle Found")
+//            }
+//        }
+//    }
+    
+    func playBoomSound() {
+        let url = URL(fileURLWithPath: Bundle.main.path(forResource: "Firework Sound.mp3", ofType: nil)!)
+        do {
+            var boomSound: AVAudioPlayer? = try AVAudioPlayer(contentsOf: url)
+            boomSound?.delegate = self
+            boomPlayer.append(boomSound!)
+            boomSound!.play()
+            boomSound = nil
+        } catch {
+            print(error)
+            print(error.localizedDescription)
+            print("Could Not Load File")
+        }
+    }
+    
+    func fireFirework(_ point: CGPoint) {
+        let randomNumber = Int(arc4random_uniform(UInt32(booms.count)))
+        if let particle = self.booms[randomNumber].copy() as? SKEmitterNode {
+            particle.run(SKAction.sequence([SKAction.wait(forDuration: 1.25), SKAction.removeFromParent()]))
+                particle.position = point
+                self.addChild(particle)
+            } else {
+                print("No Particle Found")
+            }
+
     }
     
     func getParticle(_ touch: UITouch) -> SKEmitterNode? {
@@ -46,46 +117,14 @@ class GameScene: SKScene {
             if let particle = self.booms[randomNumber].copy() as? SKEmitterNode {
                 particles[touch] = particle
                 particle.run(SKAction.sequence([SKAction.wait(forDuration: 1.25), SKAction.removeFromParent()]))
-                return particle 
+                return particle
             } else {
                 return nil
             }
         }
     }
     
-    func removeTouches(_ touches: Set<UITouch>) {
-        for touch in touches {
-            particles[touch] = nil
-            
-        }
-    }
-    
-    func fireFirework(at touches: Set<UITouch>) {
-        for touch in touches {
-            if let particle = getParticle(touch) {
-                
-                let point = touch.preciseLocation(in: touch.view)
-                particle.position = CGPoint(x: point.x, y: -point.y)
-                self.addChild(particle)
-            } else {
-                print("No Particle Found")
-            }
-        }
-    }
-    
-    func fireFirework(_ point: CGPoint) {
-        let randomNumber = Int(arc4random_uniform(UInt32(booms.count)))
-        if let particle = self.booms[randomNumber].copy() as? SKEmitterNode {
-            particle.run(SKAction.sequence([SKAction.wait(forDuration: 1.25), SKAction.removeFromParent()]))
-                
-                particle.position = point
-                self.addChild(particle)
-            } else {
-                print("No Particle Found")
-            }
-    }
     //MARK: Firework Trails
-    
     var boomTrails: [SKEmitterNode] = [SKEmitterNode(fileNamed: "testTrail")!, SKEmitterNode(fileNamed: "Trail0")!, SKEmitterNode(fileNamed: "Trail1")!, SKEmitterNode(fileNamed: "Trail2")!,SKEmitterNode(fileNamed: "Trail3")!, SKEmitterNode(fileNamed: "Trail4")!, SKEmitterNode(fileNamed: "TrailGOD")!, SKEmitterNode(fileNamed: "Trail5")!, SKEmitterNode(fileNamed: "Trail9")!,SKEmitterNode(fileNamed: "Trail10")!, SKEmitterNode(fileNamed: "Trail11")!]
     
     func startTail(at touches: Set<UITouch>, _ completion: @escaping (_ point: CGPoint) -> Void) {
@@ -93,6 +132,7 @@ class GameScene: SKScene {
             let oldPoint = touch.preciseLocation(in: self.view)
             let newPoint = CGPoint(x: oldPoint.x, y: -oldPoint.y)
         if let particleTrail = particleTrails[touch] {
+            
             if let smthT = particleTrail.copy() as? SKEmitterNode {
             smthT.run(SKAction.sequence([SKAction.move(to: newPoint, duration: TimeInterval(1.0 * (UIScreen.main.bounds.height - oldPoint.y) / UIScreen.main.bounds.height)), SKAction.removeFromParent(), SKAction.run {completion(newPoint)}]))
                 smthT.position = CGPoint(x: oldPoint.x, y: -UIScreen.main.bounds.height)
@@ -108,6 +148,7 @@ class GameScene: SKScene {
             }
         }
         }
+        startTimer()
     }
     
     override func update(_ currentTime: TimeInterval) {
@@ -118,3 +159,14 @@ class GameScene: SKScene {
         }
     }
 }
+
+extension GameScene: AVAudioPlayerDelegate {
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        guard let index = boomPlayer.index(of: player) else {
+            return
+        }
+        boomPlayer.remove(at: index)
+    }
+}
+
+
